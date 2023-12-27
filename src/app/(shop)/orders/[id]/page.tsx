ef759711a1fa509781d85eca435461445f748dfa
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-26 18:44:31 -0600

    orders pages

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,9 +1,133 @@
+import { Subtitle, Text, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { IoCardOutline } from "react-icons/io5";
 
-export default function OrderPage() {
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+interface Props {
+  params: {
+    id: string
+  }
+};
+
+export default function OrderPage({ params }: Props) {
+  const { id } = params; // todo verify
+
   return (
-    <div>
-      <h1>Order [id] Page</h1>
+    <div className="flex justify-center items-center mb-72 px-0 sm:px-10">
+      <div className="flex flex-col w-full md:w-9/12">
+        <Title>
+          Order #{id}
+        </Title>
+
+        <div className="flex flex-col md:flex-row items-start space-y-8 space-x-0 md:space-x-10 md:space-y-0">
+          {/* Carrito */}
+          <div className="flex flex-col mt-5 space-y-5">
+            <div className={
+              clsx("flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5 space-x-2",
+                {
+                  "bg-red-500": false,
+                  "bg-green-700": true
+                }
+              )
+            }>
+              <IoCardOutline size={30} />
+              <span className="">Payed</span>
+            </div>
+
+            {/* Items */}
+            <div className="space-y-4">
+              {
+                productsInCart.map((product) => (
+                  <div key={product.slug} className="flex space-x-5">
+                    <Image
+                      src={`/products/${product.images[0]}`}
+                      width={100}
+                      height={100}
+                      alt={product.title}
+                      className="rounded w-28 h-32"
+                    />
+                    <div>
+                      <Subtitle>{product.title}</Subtitle>
+                      <Text>${product.price} x 3</Text>
+                      <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+
+          {/* Checkout */}
+          <div className="bg-white rounded-xl shadow-xl p-7 space-y-8 flex-grow">
+            <div className="space-y-2">
+              <Subtitle>
+                Delivery address
+              </Subtitle>
+
+              <div>
+                <Text className="font-semibold">
+                  Victor Vazquez
+                </Text>
+                <Text>Evergreen Ave 123</Text>
+                <Text>Col. Centro</Text>
+                <Text>Alcaldia Cuauhtemoc</Text>
+                <Text>Ciudad de Mexico</Text>
+                <Text>CP: 87470</Text>
+                <Text>123.123.1234</Text>
+              </div>
+            </div>
+
+            <div className="w-full h-0.5 rounded bg-gray-200" />
+
+            <div className="space-y-2">
+              <Subtitle>
+                Order summary
+              </Subtitle>
+
+              <div className="grid grid-cols-2 gap-2">
+                <span>Products</span>
+                <span className="text-right">3 articles</span>
+
+                <span>Subtotal</span>
+                <span className="text-right">$100</span>
+
+                <span>Tax (15%)</span>
+                <span className="text-right">$15</span>
+
+                <span className="text-xl font-semibold">Total:</span>
+                <span className="text-right text-xl font-semibold">$115</span>
+              </div>
+            </div>
+
+            <div className="w-full space-y-2">
+              <div className={
+                clsx("flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5 space-x-2",
+                  {
+                    "bg-red-500": false,
+                    "bg-green-700": true
+                  }
+                )
+              }>
+                <IoCardOutline size={30} />
+                <span className="">Payed</span>
+              </div>
+
+              {/* <Link href="/orders/123" className="flex btn-primary justify-center">
+                Order
+              </Link> */}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/(shop)/orders/page.tsx
+++ src/app/(shop)/orders/page.tsx
@@ -1,9 +1,69 @@
+import { Title } from "@/components";
+import Link from "next/link";
 import React from "react";
+import { IoCardOutline } from "react-icons/io5";
 
 export default function OrdersPage() {
   return (
-    <div>
-      <h1>Orders Page</h1>
+    <div className="space-y-6">
+      <Title>Orders</Title>
+
+      <div className="mb-10">
+        <table className="min-w-full">
+          <thead className="bg-gray-200 border-b">
+            <tr>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                #ID
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Full name
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                State
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Options
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+              <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                Mark
+              </td>
+              <td className="flex items-center text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                <IoCardOutline className="text-green-800" />
+
+                <span className='mx-2 text-green-800'>Payed</span>
+              </td>
+              <td className="text-sm text-gray-900 font-light px-6 ">
+                <Link href="/orders/123" className="hover:underline">
+                  View order
+                </Link>
+              </td>
+            </tr>
+
+            <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+              <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                Mark
+              </td>
+              <td className="flex items-center text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+
+                <IoCardOutline className="text-red-800" />
+                <span className='mx-2 text-red-800'>Not payed</span>
+
+              </td>
+              <td className="text-sm text-gray-900 font-light px-6 ">
+                <Link href="/orders/123" className="hover:underline">
+                  View order
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
