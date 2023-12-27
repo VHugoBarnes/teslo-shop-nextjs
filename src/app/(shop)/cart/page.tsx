6da14da1688a13da82b9dd0767ab0f0b36ea3b98
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-26 18:12:37 -0600

    checkout page and addrees page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -12,45 +12,70 @@ const productsInCart = [
 
 export default function CartPage() {
   return (
-    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
-      <div className="flex flex-col w-2/3">
+    <div className="flex justify-center items-center mb-72 px-0 sm:px-10">
+      <div className="flex flex-col w-full md:w-9/12">
         <Title>
           Cart
         </Title>
 
-        <div className="grid grid-col-1 md:grid-col-2 space-y-8">
+        <div className="flex flex-col md:flex-row items-start space-y-8 space-x-0 md:space-x-10 md:space-y-0">
           {/* Carrito */}
           <div className="flex flex-col mt-5 space-y-5">
             <span className="text-xl">Add more items</span>
             <Link href="/" className="underline">Continue shopping</Link>
-          </div>
 
-          {/* Items */}
-          <div className="space-y-4">
-            {
-              productsInCart.map((product) => (
-                <div key={product.slug} className="flex space-x-5">
-                  <Image
-                    src={`/products/${product.images[0]}`}
-                    width={100}
-                    height={100}
-                    alt={product.title}
-                    className="rounded"
-                  />
-                  <div>
-                    <Subtitle>{product.title}</Subtitle>
-                    <Text>${product.price}</Text>
-                    <QuantitySelector quantity={3} />
-                    <button className="underline">
-                      Remove
-                    </button>
+            {/* Items */}
+            <div className="space-y-4">
+              {
+                productsInCart.map((product) => (
+                  <div key={product.slug} className="flex space-x-5">
+                    <Image
+                      src={`/products/${product.images[0]}`}
+                      width={100}
+                      height={100}
+                      alt={product.title}
+                      className="rounded w-28 h-32"
+                    />
+                    <div>
+                      <Subtitle>{product.title}</Subtitle>
+                      <Text>${product.price}</Text>
+                      <QuantitySelector quantity={3} />
+                      <button className="underline">
+                        Remove
+                      </button>
+                    </div>
                   </div>
-                </div>
-              ))
-            }
+                ))
+              }
+            </div>
           </div>
 
           {/* Checkout */}
+          <div className="bg-white rounded-xl shadow-xl p-7 space-y-4 flex-grow">
+            <Subtitle>
+              Order summary
+            </Subtitle>
+
+            <div className="grid grid-cols-2 gap-2">
+              <span>Products</span>
+              <span className="text-right">3 articles</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$100</span>
+
+              <span>Tax (15%)</span>
+              <span className="text-right">$15</span>
+
+              <span className="text-xl font-semibold">Total:</span>
+              <span className="text-right text-xl font-semibold">$115</span>
+            </div>
+
+            <div>
+              <Link href="/checkout/address" className="flex btn-primary justify-center">
+                Checkout
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,9 +1,94 @@
+import { Subtitle, Title } from "@/components";
+import Link from "next/link";
 import React from "react";
 
 export default function AddressPage() {
   return (
-    <div>
-      <h1>Address Page</h1>
+    <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
+
+      <div className="w-full md:w-9/12 flex flex-col justify-center text-left space-y-6">
+
+        <Title>Address</Title>
+        <Subtitle>Delivery address</Subtitle>
+
+        <div className="grid grid-cols-1 gap-2 sm:gap-5 sm:grid-cols-2">
+          <div className="flex flex-col mb-2">
+            <span>First name</span>
+            <input
+              type="text"
+              className="p-2 border rounded-md bg-gray-200"
+            />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Last name</span>
+            <input
+              type="text"
+              className="p-2 border rounded-md bg-gray-200"
+            />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Address</span>
+            <input
+              type="text"
+              className="p-2 border rounded-md bg-gray-200"
+            />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Address #2 (Optional)</span>
+            <input
+              type="text"
+              className="p-2 border rounded-md bg-gray-200"
+            />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Postal Code</span>
+            <input
+              type="text"
+              className="p-2 border rounded-md bg-gray-200"
+            />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>City</span>
+            <input
+              type="text"
+              className="p-2 border rounded-md bg-gray-200"
+            />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Country</span>
+            <select
+              className="p-2 border rounded-md bg-gray-200"
+            >
+              <option value="">[ Select ]</option>
+              <option value="MX">Mexico</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Phone number</span>
+            <input
+              type="text"
+              className="p-2 border rounded-md bg-gray-200"
+            />
+          </div>
+
+          <div className="flex flex-col mb-2 sm:mt-10">
+            <Link
+              href='/checkout'
+              className="btn-primary flex w-full sm:w-1/2 justify-center ">
+              Next
+            </Link>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 };
