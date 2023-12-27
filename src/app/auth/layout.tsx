e99589b00e0105e8314ed6bc7b6dc744179649bc
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-26 19:04:09 -0600

    auth pages

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function ShopLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-gray-500">
-      {children}
+    <div className="flex justify-center">
+      <div className="w-full md:w-9/12 lg:w-5/12 px-10">
+        {children}
+      </div>
     </div>
   );
 };
