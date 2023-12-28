af1c1de3e76d18316bcd10a70f52a8a1dfcbba59
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-27 18:45:10 -0600

    Update NavigationButton to use next/link

--- src/components/ui/pagination/NavigationButton.tsx
+++ src/components/ui/pagination/NavigationButton.tsx
@@ -1,16 +1,17 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 
 export const NavigationButton = ({ children, condition, href }: { children: React.ReactNode; condition: boolean; href: string; }) => {
   return (
     <li className={`page-item ${condition ? "disabled pointer-events-none" : ""}`}>
-      <a
+      <Link
         className={`${condition ? "text-gray-400" : "bg-gray-200 text-gray-700 hover:text-gray-800"} page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 focus:shadow-none`}
         href={href}
         aria-disabled={condition}
       >
         {children}
-      </a>
+      </Link>
     </li>
   );
 };
