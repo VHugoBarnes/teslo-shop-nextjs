135c195abad530af71d3c3ef9d5e1a6d225562b5
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-27 18:45:45 -0600

    Update NavigationNumberList to use next/link

--- src/components/ui/pagination/NavigationNumberList.tsx
+++ src/components/ui/pagination/NavigationNumberList.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import React from "react";
 
 export const NavigationNumberList = ({ totalPages, currentPage, path }: { totalPages: number; currentPage: number; path: string }) => {
@@ -10,12 +11,12 @@ export const NavigationNumberList = ({ totalPages, currentPage, path }: { totalP
           className={`page-item ${i + 1 === currentPage ? "active" : ""}`}
           key={i}
         >
-          <a
+          <Link
             className={`${i + 1 === currentPage ? "text-white bg-blue-500 hover:bg-blue-600" : "text-gray-800 hover:text-gray-800 hover:bg-gray-200"} page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 focus:shadow-none`}
             href={`${path}?page=${i + 1}`}
           >
             {i + 1}
-          </a>
+          </Link>
         </li>
       ))}
     </>
