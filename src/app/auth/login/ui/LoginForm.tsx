d762d039f698ca8040be2bf06037cce1f89eb81d
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-29 14:50:45 -0600

    login server action

---
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Link from "next/link";
+import { authenticate } from "@/actions";
+
+export function LoginForm() {
+  const [errorMessage, dispatch] = ReactDOM.useFormState(authenticate, undefined);
+  console.log({ errorMessage });
+
+  return (
+    <form action={dispatch} className="flex flex-col">
+      <label htmlFor="email">Email</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+        name="email"
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="password"
+        name="password"
+      />
+
+      <button
+        className="btn-primary"
+        type="submit"
+      >
+        Login
+      </button>
+
+      {/* divisor line */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-800">Or</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+      <Link
+        href="/auth/new-account"
+        className="btn-secondary text-center">
+        Create new account
+      </Link>
+    </form>
+  );
+};
